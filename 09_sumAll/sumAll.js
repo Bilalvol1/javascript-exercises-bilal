b73df14fb96620b9ece48aa61fbf7f2a10462a44
Bilal Shaikh <vol1shaikh@gmail.com>
2025-06-25 17:47:40 +0530

    pass all tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0
+    if ( a < 0 || b < 0 || 
+        a % 1 != 0 || b % 1 != 0 || 
+        typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    if (a <= b) {
+        for (let i = a; i <= b; i++) {
+        sum += i;
+        }
+    } else {
+        for (let i = a; i >= b; i--) {
+        sum += i;
+        }
+    }
+    return sum;
+}
 
-};
+console.log(sumAll(4, 1))
 
 // Do not edit below this line
 module.exports = sumAll;

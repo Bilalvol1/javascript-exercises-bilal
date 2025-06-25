ba48f8f430dd3f1ab092c543f41918a48209383e
Bilal Shaikh <vol1shaikh@gmail.com>
2025-06-25 23:29:36 +0530

    pass all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let fahrenheit = (number - 32) * (5/9);
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let celsius = (number * (9/5)) + 32;
+  return parseFloat(celsius.toFixed(1));
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(0))
+
 
 // Do not edit below this line
 module.exports = {

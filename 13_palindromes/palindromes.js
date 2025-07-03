8e9f61a5ce5b5ae50062a2de33294bd0fec4004a
Bilal Shaikh <vol1shaikh@gmail.com>
2025-07-03 17:19:08 +0530

    Resolve palindrome cases.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    
+    let splitCases = ['!', '.', ',',' '];
 
+    for (let i = 0; i < splitCases.length; i++) {
+        str = str.split(splitCases[i]).join('');
+    }
+
+    let reverse = str.split('').reverse().join('')
+    
+    return str === reverse;
 };
+
+console.log(palindromes("Hello there,!."))
 
 // Do not edit below this line
 module.exports = palindromes;

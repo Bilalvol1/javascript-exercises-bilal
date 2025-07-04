b7940bac0451120342995f21123f65ffdce2769b
Bilal Shaikh <vol1shaikh@gmail.com>
2025-07-04 18:18:06 +0530

    Pass all tests, use remote desktop on mobile

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(position) {
+    let arr = [1, 1];
+    if ( position == 0) {
+        return 0
+    } else if ( position < 0 ) {
+        return 'OOPS'
+    } else {
+        for ( let i = 2; i <= position; i++ ) {
+            arr[i] = arr[ i - 2 ] + arr [ i - 1 ];
+    }
+    return arr[position - 1 ];
+        }
+    }
+    
 // Do not edit below this line
 module.exports = fibonacci;

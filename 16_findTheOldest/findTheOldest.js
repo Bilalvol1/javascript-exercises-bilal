e6f19497045e62272579138c6afd3d6eb5a36e95
Bilal Shaikh <vol1shaikh@gmail.com>
2025-07-04 22:27:03 +0530

    Pass tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    getAge(arr);
+    sortByAge(arr);
+    return arr[0];
 };
+
+function getAge(arr) {
+    arr.forEach( value => {
+        if (!value.yearOfDeath) {
+            let currentDate = new Date();
+            return value.age = currentDate.getFullYear() - value.yearOfBirth;
+        }
+        else {
+            value.age = value.yearOfDeath - value.yearOfBirth;
+        }
+    });
+}
+
+function sortByAge(arr) {
+    return arr.sort(( a, b ) => b.age - a.age);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
